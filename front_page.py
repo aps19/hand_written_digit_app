5235eb7d447ff86270ae404dd911c94a445240bc
aps19 <s.apratap19@gmail.com>
2023-11-07 23:40:22 +0530

    second commit

---
+++ front_page.py
@@ -0,0 +1,112 @@
+import streamlit as st
+
+# About page
+def about():
+    st.title("About")
+    st.write("Welcome to the MNIST Neural Network Trainer!")
+    st.write("This app is designed to teach you how to define and train a simple Neural Network using PyTorch and skorch with Scikit-Learn. Below, we'll walk you through the process step by step.")
+
+    st.subheader("Step 1: Loading the MNIST Dataset")
+    st.write("We start by loading the MNIST dataset. The MNIST dataset contains images of handwritten digits (0-9), making it a great choice for learning computer vision and deep learning.")
+    st.write("You can learn more about the [MNIST dataset](http://yann.lecun.com/exdb/mnist/) and the transformations used in the [PyTorch documentation](https://pytorch.org/vision/stable/transforms.html).")
+    st.code("""
+    import torchvision.transforms as transforms
+    from torchvision import datasets
+
+    # Load the MNIST dataset
+    mnist = datasets.MNIST('./data', train=True, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]), download=True)
+    """)
+
+    st.subheader("Step 2: Defining the Neural Network Model")
+    st.write("Next, we define our Neural Network model using PyTorch. In this example, we'll create a simple Convolutional Neural Network (CNN).")
+    st.write("The CNN architecture used in this example consists of two convolutional layers followed by two fully connected layers.")
+    st.write("You can learn more about defining neural networks in PyTorch from the [PyTorch documentation](https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html).")
+    st.code("""
+    import torch
+    import torch.nn as nn
+    import torch.nn.functional as F
+
+    class Cnn(nn.Module):
+        def __init__(self):
+            super(Cnn, self).__init__()
+            self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+            self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+            self.fc1 = nn.Linear(1024, 128)
+            self.fc2 = nn.Linear(128, 10)
+
+        def forward(self, x):
+            x = F.relu(F.max_pool2d(self.conv1(x), 2))
+            x = F.relu(F.max_pool2d(self.conv2(x), 2))
+            x = x.view(-1, 1024)
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
+
+    model = Cnn()
+    """)
+
+    st.subheader("CNN Architecture Details")
+    st.write("In the CNN model, we have:")
+    st.write("- Two convolutional layers (conv1 and conv2) with 32 and 64 filters, respectively, both using ReLU activation.")
+    st.write("- Max-pooling layers following each convolutional layer to downsample the feature maps.")
+    st.write("- Two fully connected layers (fc1 and fc2) for classification.")
+    st.write("The choice of this architecture is common for image classification tasks, and you can experiment with different architectures for your specific problem.")
+
+
+    st.subheader("Step 3: Training the Neural Network")
+    st.write("Now, it's time to train our Neural Network model. We'll use skorch, a library that integrates PyTorch with Scikit-Learn, to make training easier.")
+    st.write("Let's dive deeper into the training process:")
+
+    st.subheader("3.1 Define Hyperparameters")
+    st.write("Before training, we need to set hyperparameters such as learning rate and the number of epochs.")
+    st.code("""
+    # Define hyperparameters
+    learning_rate = 0.001
+    max_epochs = 10
+    """)
+
+    st.subheader("3.2 Create a NeuralNetClassifier")
+    st.write("This step involves defining the model, [optimizer](https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html), [loss criterion](https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#loss-function), and other training settings.")
+    
+    st.code("""
+    from skorch import NeuralNetClassifier
+    import torch.optim as optim
+
+    # Create a NeuralNetClassifier
+    net = NeuralNetClassifier(
+        module=model,  # The model we defined in Step 2
+        optimizer=optim.Adam,  # Optimizer (e.g., Adam)
+        criterion=nn.CrossEntropyLoss,  # Loss criterion (cross-entropy for classification)
+        lr=learning_rate,  # Learning rate
+        max_epochs=max_epochs,  # Number of training epochs
+        device='cuda' if torch.cuda.is available() else 'cpu',  # Device for training (GPU or CPU)
+    )
+    """)
+    st.write("In this step, we create a `NeuralNetClassifier` using skorch. Here's what each component does:")
+    st.write("1. `module=model`: This specifies the neural network model we defined in Step 2. In this example, it's the `Cnn` class.")
+    st.write("2. `optimizer=optim.Adam`: The optimizer is responsible for adjusting the model's parameters during training to minimize the loss. In this case, we're using the Adam optimizer, which is a popular choice for deep learning tasks. You can learn more about optimizers in the [PyTorch optimization tutorial](https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html).")
+    st.write("3. `criterion=nn.CrossEntropyLoss`: The criterion (or loss function) is used to calculate the difference between the model's predictions and the actual labels. For classification tasks, cross-entropy loss is commonly used. It's a measure of how well the model's predictions match the ground truth labels. Learn more about loss functions in the [PyTorch neural networks tutorial](https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#loss-function).")
+    st.write("4. `lr=learning_rate`: The learning rate determines the size of the steps taken during optimization. It's a crucial hyperparameter to adjust for efficient training. A small learning rate may result in slow convergence, while a large one may lead to overshooting the optimal solution.")
+    st.write("5. `max_epochs=max_epochs`: This specifies the maximum number of training epochs (iterations). One epoch represents one pass through the entire training dataset. Training can stop earlier if the model converges before reaching the maximum epochs.")
+    st.write("6. `device='cuda' if torch.cuda.is available() else 'cpu'`: This sets the device for training, either GPU ('cuda') or CPU ('cpu'). Using a GPU can significantly speed up training for deep learning models. Learn more about GPU support in PyTorch in the [PyTorch documentation](https://pytorch.org/docs/stable/notes/cuda.html).")
+
+    st.write("By creating a `NeuralNetClassifier` with these settings, you've prepared the model for training. The next step is to train the model, which is covered in the following section.")
+
+
+    st.subheader("3.3 Train the Model")
+    st.write("The model is trained using the training data. Here's what the `fit` function does:")
+
+    st.write("The `fit` function trains the neural network using the provided training data (`X_train` and `y_train`). It iteratively updates the model's parameters to minimize the loss, which measures how well the model's predictions match the actual labels. This process continues for the specified number of epochs (in this case, 10) or until convergence. It uses the Adam optimizer and cross-entropy loss for training.")
+
+    st.write("You can learn more about the `fit` function and its parameters in the [skorch documentation](https://skorch.readthedocs.io/en/stable/net.html#skorch.net.NeuralNet.fit).")
+
+    st.code("""
+    # Train the model
+    net.fit(X_train, y_train)
+    """)
+
+    st.write("Congratulations! You have now learned how to set hyperparameters, create a `NeuralNetClassifier`, and train a neural network using skorch.")
+
+    st.write("You can experiment with different hyperparameters and see how they affect the training process and model performance on the 'Training and Evaluation' page.")
+
+    st.write("Happy learning!")
---
+++ training.py
@@ -0,0 +1,190 @@
+import streamlit as st
+from skorch import NeuralNetClassifier
+import random
+import torch
+import os
+import datetime
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+import torchvision.transforms as transforms
+from sklearn.datasets import fetch_openml
+
+from torchvision import datasets
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+
+# Define model as a global variable to make it accessible for all pages
+model = None
+
+
+def plot_images(X, y, rows=10, columns=10):
+    """Plot the first (rows x columns) images in a grid."""
+    fig, axes = plt.subplots(rows, columns, figsize=(12, 12))
+    
+    for i in range(rows):
+        for j in range(columns):
+            index = i * columns + j
+            ax = axes[i, j]
+            ax.imshow(X[index].reshape(28, 28), cmap='gray')
+            ax.axis('off')
+            ax.set_title(str(y[index]), fontsize=8)
+    
+    plt.tight_layout()
+    plt.show()
+
+def create_cnn_model(learning_rate, dropout, device, hidden_units, optimizer,epochs):
+    class Cnn(nn.Module):
+        def __init__(self, dropout=0.5):
+            super(Cnn, self).__init__()
+            self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+            self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+            self.conv2_drop = nn.Dropout2d(p=dropout)
+            self.fc1 = nn.Linear(1600, 100) # 1600 = number channels * width * height
+            self.fc2 = nn.Linear(100, 10)
+            self.fc1_drop = nn.Dropout(p=dropout)
+
+        def forward(self, x):
+            x = torch.relu(F.max_pool2d(self.conv1(x), 2))
+            x = torch.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+
+            # flatten over channel, height and width = 1600
+            x = x.view(-1, x.size(1) * x.size(2) * x.size(3))
+
+            x = torch.relu(self.fc1_drop(self.fc1(x)))
+            x = torch.softmax(self.fc2(x), dim=-1)
+            return x
+
+    model = NeuralNetClassifier(
+        Cnn,
+        max_epochs=epochs,  # You can set this to your preferred number of epochs
+        lr=learning_rate,
+        optimizer=optimizer,
+        device=device,
+        criterion=nn.CrossEntropyLoss,  # Use CrossEntropyLoss for classification
+        module__dropout=dropout,  # Set dropout rate as a hyperparameter
+    )
+    return model
+
+def load_mnist_data(batch_size, test_split):
+    st.subheader("Loading Dataset")
+    st.text("1. Downloading MNIST Dataset")
+    
+    data_dir = os.path.expanduser("~/data")
+
+    # Load the MNIST dataset using fetch_openml
+    mnist = fetch_openml('mnist_784', data_home=data_dir, as_frame=False, cache=True, parser='auto')
+    
+    st.text("2. Dataset Downloaded")
+
+    # Extract the data and labels
+    X = mnist.data.astype('float32')
+    y = mnist.target.astype('int64')
+    X /= 255.0  # Normalize the pixel values to [0, 1]
+
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=42)
+    
+    X_train = X_train.reshape(-1, 1, 28, 28)
+
+    st.text("3. Randomly split training and test datasets.")
+
+    # Display a 3x3 matrix of example images
+    st.subheader("Example Images")
+    example_indices = np.random.choice(len(X_train), 9, replace=False)
+    example_images = X_train[example_indices]
+    example_labels = y_train[example_indices]
+
+    fig, axes = plt.subplots(3, 3, figsize=(8, 8))
+    for i in range(3):
+        for j in range(3):
+            index = i * 3 + j
+            ax = axes[i, j]
+            ax.imshow(example_images[index][0], cmap='gray')
+            ax.axis('off')
+            ax.set_title(f"Label: {example_labels[index]}", fontsize=8)
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    return X_train,y_train
+
+
+def training_and_evaluation_app():
+    st.title("Training and Evaluation Page")
+    st.write("On this page, you can set hyperparameters, train the model, and evaluate its performance.")
+
+    # Schema of the Training Process
+    st.subheader("Training Process")
+    st.write("1. **Loading the MNIST Dataset**: The MNIST dataset is loaded and split into training and testing sets.")
+    st.write("2. **Creating the CNN Model**: A Convolutional Neural Network (CNN) is created with user-defined hyperparameters.")
+    st.write("3. **Training the Model**: The model is trained with the specified hyperparameters, such as learning rate, dropout rate, and hidden units.")
+    
+    # Hyperparameters input
+    learning_rate = st.sidebar.number_input("Learning Rate", 0.0001, 1.0, 0.001, 0.0001)
+    dropout = st.sidebar.number_input("Dropout Rate", 0.0, 0.9, 0.5, 0.1)
+    hidden_units = st.sidebar.number_input("Hidden Units", 10, 500, 100, 10)
+    optimizer = st.sidebar.selectbox("Optimizer", ["Adam", "SGD"])
+    epochs = st.sidebar.number_input("Number of Epochs",1,100,10,1)
+
+    # Allow the user to input the batch size
+    batch_size = st.sidebar.number_input("Batch Size", 1, 512, 64, 1)
+    test_split = st.sidebar.number_input("Test data size",0.01,0.5,0.2,0.01)
+
+    X_train, y_train = load_mnist_data(batch_size, test_split)  # Pass the batch size to the data loader
+    
+    optimizer = torch.optim.Adam if optimizer == "Adam" else torch.optim.SGD
+
+    # Train the model
+    if st.button("Train Model"):
+        st.write(f"Training the model with learning rate: {learning_rate}, dropout: {dropout}, hidden units: {hidden_units}, batch size: {batch_size}...")
+
+        global model
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model = create_cnn_model(learning_rate, dropout, device, hidden_units, optimizer,epochs)
+        
+        # Training results
+        st.subheader("Training Results")
+
+        for epoch in range(epochs):
+            st.write(f"Epoch {epoch + 1}/{epochs}")
+            
+            model.fit(X_train, y_train, epochs=1)  # Train for one epoch
+
+            # You can collect and display training metrics here
+            accuracy = 0.85  # Replace with your actual accuracy
+            loss = 0.12  # Replace with your actual loss
+
+            st.text(f"Accuracy: {accuracy:.2%}")
+            st.text(f"Loss: {loss:.4f}")
+
+        # Model Evaluation
+        st.subheader("Model Evaluation")
+        st.write("Evaluating the model...")
+        y_true, y_pred = [], []
+
+        with torch.no_grad():
+            for images, labels in test_loader:
+                outputs = model.predict(images)
+                y_true.extend(labels.numpy())
+                y_pred.extend(outputs.argmax(1).numpy())
+
+        accuracy = accuracy_score(y_true, y_pred)
+
+        st.write(f"Final Accuracy: {accuracy:.2%}")
+
+        conf_matrix = confusion_matrix(y_true, y_pred)
+        st.subheader("Confusion Matrix")
+        st.write(conf_matrix)
+
+        class_report = classification_report(y_true, y_pred)
+        st.subheader("Classification Report")
+        st.text(class_report)
+
+        plt.figure(figsize=(8, 6))
+        plt.imshow(conf_matrix, cmap="Blues", interpolation="nearest")
+        st.subheader("Confusion Matrix Heatmap")
+        st.pyplot(plt)
+
